b56636ee20d36a3df0cfe7f834de89a5d0be9fa6
Pedro <psilva@cern.ch>
2021-05-04 11:06:41 +0200

    fixing type

--- templates/partGun_GSD_template.py
+++ templates/partGun_GSD_template.py
@@ -109,6 +109,6 @@ elif gunmode == 'physproc':
         process.FEVTDEBUGHLToutput.SelectEvents.SelectEvents=cms.vstring(filterPath.label())
 
 # enable customisation of CE-H noise and sipm type
-process.mix.digitizers.hgchebackDigitizer.digiCfg.pxFiringRate  = cms.untracked.uint32(DUMMYPXFIRINGRATE)
+process.mix.digitizers.hgchebackDigitizer.digiCfg.pxFiringRate  = cms.double(DUMMYPXFIRINGRATE)
 process.mix.digitizers.hgchebackDigitizer.digiCfg.sipmMap       = cms.string(DUMMYSIPMAP)
 
